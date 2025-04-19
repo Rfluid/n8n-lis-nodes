5d9e8638a2249bba57c0c85ffa5c6f83d5c1ce65
Rfluid <ruy.vieiraneto@gmail.com>
2025-04-19 17:33:29 -0300

    fix: `build:icons`

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, series } = require('gulp');
 
-task('build:icons', copyIcons);
-
-function copyIcons() {
+function copyNodeIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
+	return src(nodeSource).pipe(dest(nodeDestination));
+}
 
-	src(nodeSource).pipe(dest(nodeDestination));
-
+function copyCredentialIcons() {
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
-
 	return src(credSource).pipe(dest(credDestination));
 }
+
+function copyAssetIcons() {
+	const assetSource = path.resolve('assets', 'icons', '**', '*.{png,svg}');
+	const assetDestination = path.resolve('dist', 'assets', 'icons');
+	return src(assetSource).pipe(dest(assetDestination));
+}
+
+task('build:icons', series(copyNodeIcons, copyCredentialIcons, copyAssetIcons));
